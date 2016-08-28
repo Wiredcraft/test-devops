e031add26d424144ea9bbeff2f7972d9e446bff8
stefan.bu2 <stefan@ub2.com>
2016-08-28 21:41:23 +0800

    fix bugs

--- ops/start.sh
+++ ops/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd playbooks/ && ansible-playbook -e 'host_key_checking=False' -i staging init-deploy.yml 
+cd playbooks/ && ansible-playbook -e 'host_key_checking=False' -i staging main.yml 
