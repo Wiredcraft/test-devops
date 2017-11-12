1a7dac44c6adeb8f412d4ceef031275991442568
shenyi <yi.shen@arkdesign.cn>
2017-11-12 22:43:00 +0800

    qqq

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 apt-get install -y unzip
-mkdir -p /opt/hugo/{dev, staging}
+mkdir -p /opt/hugo/dev
 cd /opt/hugo/dev
 wget https://github.com/slidemoon/test-devops/archive/shenyi.zip
 unzip ./shenyi.zip
 cd ./test-devops-shenyi/quickstart
 nohup hugo server -D -p 1313 &
+mkdir -p /opt/hugo/staging
 cd /opt/hugo/staging
 wget https://github.com/slidemoon/test-devops/archive/shenyi.zip
 unzip ./shenyi.zip
