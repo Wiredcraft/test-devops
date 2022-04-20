67c675d681ca6184c384843f8fa7d33bda95734b
卢克利 <keli.lu@iquantex.com>
2022-04-20 13:43:43 +0800

    Improve Task 4

--- task4/apicode.py
+++ task4/apicode.py
@@ -2,18 +2,21 @@ from flask import Flask, request, jsonify, session
 import redis
 
 app = Flask(__name__)
+
+# Set redis config
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
 
+# Get Method, if key(name) not exist, return "Hello, Wiredcraft!"
 @app.route('/welcome', methods=["GET"])
-def Get():
-    name = request.args.get('name')
-    if(name):
-        return "Hello, " + name +"!"
+def Get_Name():
+    if(cache.get('name')):
+        return "Hello, " + cache.get('name').decode() + "!"
     return "Hello, Wiredcraft!"
 
+# Put Method, used to set key/value
 @app.route('/welcome', methods=["PUT"])
-def Put():
+def Put_Name():
     name = request.json.get("name")
-    cache.set('name',name)
-    return "Put Success"
+    cache.set('name', name)
+    return "Put Success, Name is " + name
 
